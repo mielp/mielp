39b1ea6750a5b399a28cf4d32e3c490572e247a1
Miel <miel+git@lly.fr>
2020-06-27 16:35:16 +0200

    Change the emoji on light & dark modes

--- frills/home.js
+++ frills/home.js
@@ -5,7 +5,7 @@
  * Basic toggle for dark and light modes
  */
 
-let lightToggle = document.getElementById('twilight');
+let btn = document.getElementById('twilight');
 let query = window.matchMedia('(prefers-color-scheme: dark)');
 let classList = document.documentElement.classList;
 let isDark = query.matches;
@@ -13,11 +13,14 @@ let isDark = query.matches;
 let toggle = function (dark) {
   isDark = dark;
   classList.toggle('dark', isDark);
+  if (btn !== undefined) {
+    btn.innerText = isDark ? '🌙' : '☀️';
+  }
 };
 
 query.addListener(e => toggle(e.matches));
 toggle(isDark);
 
-if (lightToggle !== undefined) {
-  lightToggle.addEventListener('click', e => toggle(!isDark));
+if (btn !== undefined) {
+  btn.addEventListener('click', e => toggle(!isDark));
 }
